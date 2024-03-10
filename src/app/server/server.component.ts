42e84100290e52a8d6ee8369b8cab4620a91f2a8
Tom Biato <biatotom@gmail.com>
2024-03-10 12:53:25 +0100

    ⚙️ refactor(server): update server component functionality
    - Refactored the server component to dynamically set the server status and color based on a random value
    - Added a getColor method to determine the color of the server status
    - Updated the server component HTML template to display the server status with appropriate styling
    - Updated the server component CSS to style the online status text
    - Updated the servers component to display a list of servers and handle server creation with a dynamic message
    
    #server #refactor #css #html #angular

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,5 +9,13 @@ export class ServerComponent {
   serverId: number = 10;
   serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus = () => this.serverStatus;
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
   serverName: any = '';
+  serverCreated: boolean = false;
+  servers: string[] = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -17,11 +19,13 @@ export class ServersComponent {
   }
 
   onCreateServer = () => {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
-    console.log(event);
+    // console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
